b82fc88e7bb3ce62a4035f67d769262541203dc5
Artem Averin <artem-averin-91@mail.ru>
2023-06-19 17:44:48 +0300

    8. Скачивание файла с ограничением. [#144271 # [#144271]]

---
+++ src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ThreadStop {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("start ...");
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
+        thread.join();
+
+    }
+}

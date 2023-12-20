62bbbca3603d94c57b35b1e26ed9e2a672216cf9
Artem Averin <artem-averin-91@mail.ru>
2023-12-20 14:42:32 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,44 @@
+package ru.job4j.buffer;
+
+import ru.job4j.wait.SimpleBlockingQueue;
+
+public class ParallelSearch {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(5);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+
+
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+        );
+        consumer.start();
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+    }
+}

6a9a164a99559e1beaa6b81b8b7a12be2188f1f0
Artem Averin <artem-averin-91@mail.ru>
2023-12-21 23:32:27 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,36 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) throws OptimisticException {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        Base stored = memory.get(model.id());
+        if (stored.version() != model.version()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        int version = model.version() + 1;
+        return memory.computeIfPresent(model.id(),
+                (x, y) -> new Base(model.id(), model.name(), version)) != null;
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+
+}

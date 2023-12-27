3159da602125fd1dff7dafe21d2a82126fd073a0
Artem Averin <artem-averin-91@mail.ru>
2023-12-27 13:27:03 +0300

    3. ForkJoinPool [#315067]

---
+++ src/main/java/ru/job4j/pools/MyParallelSearch.java
@@ -0,0 +1,59 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+public class MyParallelSearch<T> extends RecursiveTask<Integer> {
+
+    private final T object;
+    private final T[] array;
+    private  int from;
+    private  int to;
+
+    public MyParallelSearch(T object, T[] array, int from, int to) {
+        this.object = object;
+        this.array = array;
+        this.from = from;
+        this.to = to;
+    }
+
+    public MyParallelSearch(T object, T[] array) {
+        this.object = object;
+        this.array = array;
+    }
+
+    @Override
+    protected Integer compute() {
+        if ((to - from) <= 10) {
+            return search(from, to);
+        }
+        int middle = (from + to) / 2;
+
+        MyParallelSearch<T> first = new MyParallelSearch<>(object, array, from, middle + 1);
+        MyParallelSearch<T> second = new MyParallelSearch<>(object, array, middle + 1, to);
+
+        first.fork();
+        second.fork();
+
+        Integer firstJoin = first.join();
+        Integer secondJoin = second.join();
+
+        return Math.max(firstJoin, secondJoin);
+    }
+
+    public Integer search(int from, int to) {
+        int res = -1;
+        for (int i = from; i <= to; i++) {
+            if (array[i].equals(object)) {
+                res = i;
+            }
+        }
+        return res;
+    }
+
+    public Integer start() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new MyParallelSearch<>(object, array, 0, array.length - 1));
+    }
+
+}
---
+++ src/test/java/ru/job4j/pools/MyParallelSearchTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.pools;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MyParallelSearchTest {
+
+
+    @Test
+    void whenIntegerType(){
+        Integer[] array = new Integer[50];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        MyParallelSearch<Integer> search = new MyParallelSearch<>(25, array);
+        assertThat(search.start()).isEqualTo(24);
+    }
+
+    @Test
+    void whenStringType(){
+        String[] array = new String[50];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = String.valueOf(i + 1);
+        }
+        MyParallelSearch<String> search = new MyParallelSearch<>("25", array);
+        assertThat(search.start()).isEqualTo(24);
+    }
+
+    @Test
+    void whenLineaSearch(){
+        Integer[] array = new Integer[9];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        MyParallelSearch<Integer> search = new MyParallelSearch<>(4, array);
+        assertThat(search.start()).isEqualTo(3);
+    }
+
+    @Test
+    void whenParallelSearch(){
+        Integer[] array = new Integer[10000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        MyParallelSearch<Integer> search = new MyParallelSearch<>(9999, array);
+        assertThat(search.start()).isEqualTo(9998);
+    }
+
+    @Test
+    void whenObjectNotFound(){
+        Integer[] array = new Integer[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        MyParallelSearch<Integer> search = new MyParallelSearch<>(666, array);
+        assertThat(search.start()).isEqualTo(-1);
+    }
+
+}
